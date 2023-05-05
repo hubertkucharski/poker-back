f4a879851c15bd02366e321ba81d168f1a8a57a5
hubertkucharski <hubertkucharski@gmail.com>
2023-05-05 20:44:16 +0200

    add cors

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config } from './../config/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.enableCors({
-  //   allowedHeaders: ['content-type'],
-  //   origin: localConfig.corsOrigin,
-  //   credentials: true,
-  // });
+  app.enableCors({
+    allowedHeaders: ['content-type'],
+    origin: config.corsOrigin,
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
